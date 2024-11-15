954626c2095bee199ab9d8b2a04868ffb0f35c33
Udit Saurabh <uditsaurabh747@gmail.com>
2024-11-15 09:54:25 +0530

    adding navbar code

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,27 @@
+"use client"
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { FaGoogle } from "react-icons/fa"
 import profile from "@/assets/images/profile.png"
 import logoDefault from "@/assets/images/logo-white.png"
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false)
     return (
         <nav className="bg-blue-700 border-b border-blue-500">
             <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-20 items-center justify-between">
                     <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
-
                         <button
                             type="button"
                             id="mobile-dropdown-button"
                             className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                             aria-controls="mobile-menu"
                             aria-expanded="false"
+                            onClick={() => setIsOpen(!isOpen)}
                         >
                             <span className="absolute -inset-0.5"></span>
+                            <FaGoogle className='mr-2 text-white' />
                             <span className="sr-only">Open main menu</span>
                             <svg
                                 className="block h-6 w-6"
@@ -36,11 +39,9 @@ function Navbar() {
                             </svg>
                         </button>
                     </div>
-
                     <div
                         className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
                     >
-
                         <Link className="flex flex-shrink-0 items-center" href="/">
                             <Image
                                 className="h-8 w-8 rounded-full"
@@ -49,12 +50,10 @@ function Navbar() {
                                 width={32}
                                 height={32}
                             />
-
                             <span className="hidden md:block text-white text-2xl font-bold ml-2"
                             >PropertyPulse</span
                             >
                         </Link>
-
                         <div className="hidden md:ml-6 md:block">
                             <div className="flex space-x-2">
                                 <Link
@@ -75,20 +74,16 @@ function Navbar() {
                             </div>
                         </div>
                     </div>
-
-
                     <div className="hidden md:block md:ml-6">
                         <div className="flex items-center">
                             <button
-                                className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+                                className="flex items-center text-purple-400 bg-lime-300 hover:bg-gray-900 hover:text-orange-400 rounded-md px-3 py-2"
                             >
-                                <i className="fa-brands fa-google text-white mr-2"></i>
-                                <span>Login or Register</span>
+                                <FaGoogle className='mr-2' />
+                                <span>Google Login or Register</span>
                             </button>
                         </div>
                     </div>
-
-
                     <div
                         className="absolute inset-y-0 right-0 flex items-center pr-2 md:static md:inset-auto md:ml-6 md:pr-0"
                     >
@@ -118,7 +113,6 @@ function Navbar() {
                                 className="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-600 rounded-full"
                             >
                                 2
-
                             </span>
                         </Link>
 
@@ -142,8 +136,6 @@ function Navbar() {
                                     />
                                 </button>
                             </div>
-
-
                             <div
                                 id="user-menu"
                                 className="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
@@ -178,8 +170,6 @@ function Navbar() {
                     </div>
                 </div>
             </div>
-
-
             <div className="hidden" id="mobile-menu">
                 <div className="space-y-1 px-2 pb-3 pt-2">
                     <Link
@@ -196,10 +186,10 @@ function Navbar() {
                         className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
                     >Add Property</Link>
                     <button
-                        className="flex items-center text-white bg-amber-400 hover:bg-amber-300 hover:text-white rounded-md px-3 py-2 my-4"
+                        className="flex items-center rounded-md px-3 py-2 my-4"
                     >
-                        <FaGoogle className='mr-2 text-white h-4 w-4' />
-                        <span>Login or Register</span>
+                        <span>Google</span>
+                        <span> Google Login or Register</span>
                     </button>
                 </div>
             </div>
